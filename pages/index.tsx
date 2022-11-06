4138566b388323870883a5946d7829cc83dcd035
Sam Magura <srmagura@gmail.com>
2022-11-06 12:47:16 -0500

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
   return (
-    <Link href="xyz" legacyBehavior>
+    <Link href="xyz">
       <a>
         <span>A</span> &nbsp; B
       </a>
