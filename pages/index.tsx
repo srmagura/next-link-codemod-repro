903e7ff376b496db9963367c5de4fb6c727316d6
Sam Magura <srmagura@gmail.com>
2022-11-06 12:45:35 -0500

    Add link

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,10 @@
+import Link from "next/link";
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return (
+    <Link href="xyz">
+      <span>A</span> &nbsp; B
+    </Link>
+  );
 }
